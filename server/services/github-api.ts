2fe0011658160f960d3810fa033b710e8940920a
untukqween-dev <untukqween@gmail.com>
2026-05-30 11:27:24 +0700

    server: Add GitHub services placeholder

---
+++ server/services/github-api.ts
@@ -0,0 +1,8 @@
+// GitHub API Services
+// TODO: Implement GitHub OAuth flow
+// TODO: Implement repository listing
+// TODO: Implement Codespaces management
+
+export const githubServices = {
+  // Placeholder for GitHub service implementations
+};
