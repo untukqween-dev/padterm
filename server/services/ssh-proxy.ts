c7b85ec544d9ddf9cd8b191b4fd3b99b23721b51
untukqween-dev <untukqween@gmail.com>
2026-05-30 11:25:58 +0700

    server: Add SSH proxy placeholder

---
+++ server/services/ssh-proxy.ts
@@ -0,0 +1,8 @@
+// SSH Proxy Service
+// TODO: Implement SSH connection management
+// TODO: Implement PTY allocation
+// TODO: Implement session management
+
+export const sshProxy = {
+  // Placeholder for SSH proxy implementations
+};
